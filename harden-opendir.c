48119cae10948f1312d9d0c458b56af850e0955f
Christopher M Wagner <70287489+Cwagne17@users.noreply.github.com>
2022-11-19 18:30:06 -0500

    wrapper function finished

--- harden-opendir.c
+++ harden-opendir.c
@@ -1,45 +1,70 @@
 #define _GNU_SOURCE
-#include <stdlib.h>
-#include <stdio.h>
-#include <dlfcn.h>
-
-/**
- * Types needed for opendir
- * See `man opendir`.
-*/
 #include <dirent.h>
-#include <unistd.h>
-#include <string.h>
+#include <dlfcn.h>
 #include <errno.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <syslog.h>
+#include <unistd.h>
 
-#define HOME_PATH "/home"
+#define HOME_PATH "/home" // string used to compare the target path of `ls`.
 
 static DIR* (*real_opendir)(const char *name) = NULL;
 
 DIR *opendir(const char *name) {
-	// First-time initialization: get a pointer to the real memcpy 
+	// get a pointer to the real opendir 
 	if (real_opendir == NULL) {
-		char *error;
-		real_opendir = dlsym(RTLD_NEXT, "opendir");
+		/**
+		 * From dlsym(3).
+		 * 
+		 * Finds the next occurrence of the opendir symbol in the
+		 * search order after the current object. This allows one to 
+		 * provide a wrapper around a function in another shared 
+		 * object, so that, for example, the definition of a function
+		 * in a preloaded shared object (see LD_PRELOAD in ld.so(8))
+		 * can find and invoke the "real" function provided in
+		 * another shared object (or for that matter, the "next"
+		 * definition of the function in cases where there are
+		 * multiple layers of preloading).
+		*/
+		real_opendir = dlsym(RTLD_NEXT, "opendir"); // obtain address of a symbol in a shared object or executable
 		
-		/* If we couldn't find the real opendir, abort with a message */
-		error = dlerror();
-		if (error != NULL) {
+		/* if real opendir isn't resolved, abort with a msg */
+		char *error;
+		if ((error = dlerror()) != NULL) {
 			fprintf(stderr, "%s\n", error);
-			exit(1);
+			exit(EXIT_FAILURE);
 		}
 	}
 
-	char *dname;
-	dname = (char*)name; // copies value of name pointer into dname
-	if (strcmp(name, ".") == 0) { // ls is for cwd then get current dir name
-		dname = get_current_dir_name();
+	char tpath[PATH_MAX];
+	char *res = realpath(name, tpath); // sets tpath to resolved path
+	if (res == NULL) {
+        perror("realpath"); // prints descriptive err msg to stderr
+        exit(EXIT_FAILURE);
 	}
-	if (strncmp(dname, HOME_PATH, strlen(HOME_PATH)) != 0) {
-		errno = EACCES;
 
-		// add code to send err log to syslog
-		return NULL;
+	// stmt is true if tpath does not begin with "/home"
+	if (strncmp(tpath, HOME_PATH, strlen(HOME_PATH)) != 0) {
+		openlog(
+			"libharden", 	// identity when opening log
+			LOG_CONS | 		// err to system console if err sending to syslog
+			LOG_ODELAY | 	// waits to open conn until syslog() invoked
+			LOG_PID, 		// includes PID in msg
+			LOG_AUTH 		// labels msg as security/auth
+		);
+		syslog(
+			LOG_AUTH | 		// labels msg as security/auth
+		 	LOG_ERR, 		// labels msg as an error
+			"UID %d tried using ls with the target %s\n", // formats msg for /var/log/auth.log
+			getuid(), tpath // input for formatted msg
+		);
+		closelog();
+
+		errno = EACCES; // set error no. to Permission Denied
+		return NULL; 	// ret NULL to invoke the previous syscall to handle EACCES (exits command)
 	}
 
 	/* Call the real opendir with the same arguments. */
